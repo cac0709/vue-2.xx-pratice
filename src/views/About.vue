ba36fa88a9b3df642a05989e926b979bca95cff7
unknown <user@ITA03-HQNB3-109.tpech.gov.tw>
2021-02-23 15:51:23 +0800

    vue2.x

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <AboutText/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import AboutText from '@/components/AboutText.vue'
+
+export default {
+  name: 'About',
+  components: {
+    AboutText
+  }
+}
+</script>
+
---
+++ src/views/Pratice.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pratice">
+    <h1>表單</h1>   
+     <PraticeText/>
+     
+
+     
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import PraticeText from '@/components/PraticeText.vue'
+
+export default {
+  name: 'Pratice',
+  components: {
+    PraticeText
+  }
+}
+
+</script>
